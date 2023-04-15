0456e9afe6d2b7cf4b774bb64b351807d7070bc4
Julian Lechner <jll38@njit.edu>
2023-04-15 15:56:11 -0400

    social box, page formatting, styles

---
+++ src/components/socialbox.js
@@ -0,0 +1,20 @@
+import socialstyle from '../styles/socialbox.module.css';
+
+import Link from 'next/link';
+import Head from 'next/head';
+export default function SocialBox() {
+    return (
+        <>
+            <Head>
+                <script src="https://kit.fontawesome.com/a7908c27f8.js" crossorigin="anonymous"></script>
+            </Head>
+            <div className={`${socialstyle.container}`}>
+            <a href='https://www.linkedin.com/in/julianllechner/' target={'_blank'}><i className="fab fa-linkedin fa-2xl text-pink-600 mb-4"></i></a>
+                <br/>
+                <a href='https://github.com/jll38' target={'_blank'}><i className="fab fa-github fa-2xl text-pink-600"></i></a>
+                <div className={`${socialstyle.divider}`}></div>
+            </div>
+
+        </>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,9 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import BGAnim from '@/components/BG-Anim';
 import Navbar from '@/components/Navbar';
+import SocialBox from '@/components/socialbox';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +19,7 @@ export default function Home() {
     <>
       <main id="main" className="flex min-h-screen flex-col items-center p-6 md:p-24 content-center justify-center">
         <Navbar></Navbar>
+        <SocialBox/>
         <div className="z-10">
           <h1 className="text-4xl md:text-6xl text-gray-600 dark:text-gray-300">Hi Im</h1>
           <div className="text-transparent bg-clip-text bg-gradient-to-t from-purple-500 to-red-700">
@@ -30,26 +33,33 @@ export default function Home() {
           </button>
         </div>
       </main>
-      <section name="about" className="bg-black min-h-screen flex flex-col px-6 md:px-72">
-        <div className="flex flex-wrap">
-          <h2 className="text-3xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-purple-500 to-red-700 divider">01. About Me</h2>
+      <section name="about" className="bg-black min-h-screen flex px-6 md:px-72">
+        <div className="w-3/4">
+          <div className="flex flex-wrap">
+            <h2 className="text-3xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-purple-500 to-red-700 divider">01. About Me</h2>
+          </div>
+          <div>
+            <div name="about-text" className="text-lg mt-8">
+              <p>
+                Hello! My name is <span className="text-transparent bg-clip-text bg-gradient-to-t from-purple-500 to-red-700 font-bold">Julian Lechner</span> and I enjoy building
+                web applications.
+              </p>
+              <br />
+              <p>Here are a few of the technologies I&apos;ve been using recently</p>
+              <ul name="skill-list" className="skill-list">
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Flask</li>
+                <li>Docker</li>
+                <li>SQL</li>
+                <li>Playwright</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div name="about-text" className="text-lg mt-8">
-          <p>
-            Hello! My name is <span className="text-transparent bg-clip-text bg-gradient-to-t from-purple-500 to-red-700 font-bold">Julian Lechner</span> and I enjoy building
-            web applications.
-          </p>
-          <br />
-          <p>Here are a few of the technologies I&apos;ve been using recently</p>
-          <ul name="skill-list" className="skill-list">
-            <li>React</li>
-            <li>Node.js</li>
-            <li>Flask</li>
-            <li>Docker</li>
-            <li>SQL</li>
-            <li>Playwright</li>
-          </ul>
-        </div>
+        <div className="w-1/4">
+            <img className="object-fill" src="images/myself.png"></img>
+          </div>
       </section>
     </>
   );
