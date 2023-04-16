d41297bc47b90f0a36d1dc74c9b9e2a818e78185
Julian Lechner <jll38@njit.edu>
2023-04-16 14:09:04 -0400

    scroll reef

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { Sectionheader } from './../components/sectionheader';
+import {useRef} from 'react';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import BGAnim from '@/components/BG-Anim';
@@ -12,6 +13,19 @@ const inter = Inter({ subsets: ['latin'] });
 
 
 export default function Home() {
+  const aboutRef = useRef(null);
+  const featRef = useRef(null);
+
+  //Smooth scrolls down to about section
+  const handleArrow1Click = () => {
+    aboutRef.current?.scrollIntoView({behavior: 'smooth'})
+  };
+
+  //Smooth scrols to project section
+  const handleArrow2Click = () => {
+    featRef.current?.scrollIntoView({behavior: 'smooth'})
+  };
+  
   return (
     <>
       <main id="main" className="flex min-h-screen flex-col items-center p-6 md:p-24 content-center justify-center">
@@ -26,10 +40,10 @@ export default function Home() {
           <p className="mb-4 text-xl font-normal text-gray-600 dark:text-gray-300">
             Responsive, <span className="text-transparent bg-clip-text bg-gradient-to-t from-purple-500 to-red-700">Reliable</span>, and Robust Web Development.</p>
         </div>
-        <i name='down-arrow' class={`${animations.bob} fa-solid fa-angle-down text-purple-400`}></i>
+        <button onClick={handleArrow1Click}><i  name='down-arrow' class={`${animations.bob} fa-solid fa-angle-down fa-xl text-purple-400`}></i></button>
       </main>
 
-      <section name="about" className="min-h-screen flex-col px-16 md:px-48 content-center justify-center items-center">
+      <section ref={aboutRef} name="about" className="flex min-h-screen flex-col px-16 md:px-48 content-center justify-center items-center">
         <div className='flex flex-wrap mt-20'>
           <div className="w-full md:w-1/2">
             <Sectionheader title={'About Me'}/>
@@ -66,9 +80,11 @@ export default function Home() {
             <img className="w-3/4 h-auto m-auto rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1" src="images/myself.png"></img>
           </div>
         </div>
+        <button onClick={handleArrow2Click}><i  name='down-arrow' class={`${animations.bob} fa-solid fa-angle-down fa-xl text-purple-400 m-auto mt-10`}></i></button>
+
       </section>
 
-      <section name="features" className="min-h-screen flex-col px-16 md:px-48 content-center justify-center items-center">
+      <section ref={featRef} name="features" className="flex min-h-screen flex-col px-16 md:px-48 content-center justify-center items-center">
           <Sectionheader title={'Featured Projects'}/>
           <div name='project-showcase' className='w-full m-5 grid grid-cols-2 gap-10'>
               
