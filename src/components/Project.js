354c66e46ee884b799b55b34d5582b8df9b71402
Julian Lechner <jll38@njit.edu>
2023-04-20 17:33:31 -0400

    Homepage styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,11 +4,11 @@ import ScaleOnHover from "./ScaleOnHover";
 
 const Project = (props) => {
   return (
-    <div className="my-2 w-1/2">
+    <div className="my-2">
       <div className="flex gap-4">
         <ScaleOnHover>
           <a href={props.link} target="_blank">
-            <h4 className="text-4xl text-transparent bg-clip-text bg-gradient-to-t from-purple-900 via-red-500 to-yellow-500 font-semibold">
+            <h4 className="text-2xl xl:text-4xl text-transparent bg-clip-text bg-gradient-to-t from-purple-900 via-red-500 to-yellow-500 font-semibold">
               {props.name}
             </h4>
           </a>
