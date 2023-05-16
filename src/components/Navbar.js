bfb1cd1df8d7c3b477ded185d431221b50021361
Julian Lechner <jll38@njit.edu>
2023-05-16 18:44:53 -0400

    Navbar Buttons ScaleOnHover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,48 @@
-import navstyle from '../styles/navbar.module.css';
-import Link from 'next/link';
+import navstyle from "../styles/navbar.module.css";
+import Link from "next/link";
 import { useTheme } from "next-themes";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import ScaleOnHover from "./ScaleOnHover";
+
 export default function Navbar() {
-    const [mounted, setMounted] = useState(false);
-    const {theme, setTheme} = useTheme();
-    useEffect(() => setMounted(true), [])
-    if(!mounted) return null;
-    return (
-        <div className={`${navstyle.container} flex flex-row flex-wrap justify-between`}>
-            <div>
-                <Link href='/' className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-radial from-blue-600 to-blue-400 dark:bg-gradient-to-t dark:from-purple-500 dark:to-red-700">JL</Link>
-            </div>
-            <div className='flex gap-5'>
-                <button
-                className='w-[30px] dark:text-pink-500 border-2 rounded-lg text-blue-400 border-blue-400 dark:border-pink-500'
-                onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-                    {theme === 'light' ? (<i class="fa-solid fa-moon"></i>) : (<i class="fa-solid fa-sun"></i>)}
-                </button>
-                <Link href='/blog' className="text-xl font-bold text-transparent bg-clip-text bg-gradient-radial from-blue-600 to-blue-400 dark:bg-gradient-to-t dark:from-purple-500 dark:to-red-700">Blog</Link>
-            </div>
-        </div>
-    );
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+  return (
+    <div
+      className={`${navstyle.container} flex flex-row flex-wrap justify-between`}
+    >
+      <ScaleOnHover>
+        <Link
+          href="/"
+          className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-radial from-blue-600 to-blue-400 dark:bg-gradient-to-t dark:from-purple-500 dark:to-red-700"
+        >
+          JL
+        </Link>
+      </ScaleOnHover>
+      <div className="flex gap-5">
+        <ScaleOnHover>
+          <button
+            className="w-[30px] dark:text-pink-500 border-2 rounded-lg text-blue-400 border-blue-400 dark:border-pink-500"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            {theme === "light" ? (
+              <i class="fa-solid fa-moon"></i>
+            ) : (
+              <i class="fa-solid fa-sun"></i>
+            )}
+          </button>
+        </ScaleOnHover>
+        <ScaleOnHover>
+          <Link
+            href="/blog"
+            className="text-xl font-bold text-transparent bg-clip-text bg-gradient-radial from-blue-600 to-blue-400 dark:bg-gradient-to-t dark:from-purple-500 dark:to-red-700"
+          >
+            Blog
+          </Link>
+        </ScaleOnHover>
+      </div>
+    </div>
+  );
 }
--- src/components/socialbox.js
+++ src/components/socialbox.js
@@ -1,20 +1,45 @@
-import socialstyle from '../styles/socialbox.module.css';
-
-import Link from 'next/link';
-import Head from 'next/head';
+import socialstyle from "../styles/socialbox.module.css";
+import ScaleOnHover from "./ScaleOnHover";
+import Link from "next/link";
+import Head from "next/head";
 export default function SocialBox() {
-    return (
-        <>
-            <Head>
-                <script defer src="https://kit.fontawesome.com/a7908c27f8.js" crossorigin="anonymous"></script>
-            </Head>
-            <div className={`${socialstyle.container}`}>
-            <a href='https://www.linkedin.com/in/julianllechner/' target={'_blank'} aria-label='LinkedIn Link'><i className="fab fa-linkedin fa-2xl text-blue-600 dark:text-pink-600 mb-4" alt='linkedin logo'></i></a>
-                <br/>
-                <a href='https://github.com/jll38' target={'_blank'} aria-label='Github Link'><i className="fab fa-github fa-2xl text-blue-600 dark:text-pink-600" alt='github logo'></i></a>
-                <div className={`dark:${socialstyle.divider}`}></div>
-            </div>
-
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <script
+          defer
+          src="https://kit.fontawesome.com/a7908c27f8.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <div className={`${socialstyle.container}`}>
+        <ScaleOnHover>
+          <a
+            href="https://www.linkedin.com/in/julianllechner/"
+            target={"_blank"}
+            aria-label="LinkedIn Link"
+          >
+            <i
+              className="fab fa-linkedin fa-2xl text-blue-600 dark:text-pink-600 mb-4"
+              alt="linkedin logo"
+            ></i>
+          </a>
+        </ScaleOnHover>
+        <br />
+        <ScaleOnHover>
+          <a
+            href="https://github.com/jll38"
+            target={"_blank"}
+            aria-label="Github Link"
+          >
+            <i
+              className="fab fa-github fa-2xl text-blue-600 dark:text-pink-600"
+              alt="github logo"
+            ></i>
+          </a>
+        </ScaleOnHover>
+        <div className={`dark:${socialstyle.divider}`}></div>
+      </div>
+    </>
+  );
 }
